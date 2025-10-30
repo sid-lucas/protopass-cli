c37916f7df5debdfe260cd37335122125fc405d9
sid-lucas <pro.github.rojvi@slmails.com>
2025-10-30 11:34:48 +0100

    Add commands test

--- cli.py
+++ cli.py
@@ -3,13 +3,30 @@ import argparse
 def main():
     # Création parseur de commandes
     parser = argparse.ArgumentParser(
-        prog="protoncli",
+        prog="protonpass-cli",
         description="Prototype password manager CLI"
     )
 
+    # Espace pour des sous-commandes (init, login, etc.)
+    subparsers = parser.add_subparsers(dest="command", help="Sous-commandes")
+
+    # déclaration sous-commande 'init'
+    p_init = subparsers.add_parser("init", help="Initialiser le coffre")
+
+    # association fonction à 'init'
+    p_init.set_defaults(func=lambda _args: print("Commande 'init' appelée."))
+
+
+    # Lit ce que l'user passe comme arguments (après 'python cli.py')
     args = parser.parse_args()
 
-    print("CLI prêt (aucune commande définie).")
+    if not args.command:
+        parser.print_help()
+        return
+
+    # appelle la fonction associée à la sous-commande
+    args.func(args)
+
 
 if __name__ == "__main__":
     main()
